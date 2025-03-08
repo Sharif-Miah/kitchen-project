63b080538ead9aa049b890388bc8b19927fceeb1
Sharif-Miah <farhansharif1410@gmail.com>
2025-03-08 17:58:06 +0600

    hero section added

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import Hero from '@/components/Hero';
+
 export default function Home() {
   return (
-    <div>
-      <h1>This is kitchen project</h1>
-    </div>
+    <main className='container mx-auto px-4 mt-[100px]'>
+      <Hero />
+    </main>
   );
 }
